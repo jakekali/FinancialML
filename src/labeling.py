0da37d1018769e8d19c43e37ce38fc22c59366e2
Jacob Khalili <jacobkal18@gmail.com>
2023-03-12 16:30:43 -0400

    triple barrier new attempt

--- src/labeling.py
+++ src/labeling.py
@@ -2,6 +2,8 @@ import pandas as pd
 from abc import abstractmethod
 import multiprocessing as mp
 import datetime as dt
+import tqdm
+
 
 class labeler:
     '''
@@ -96,7 +98,7 @@ class tripleBarrierLabeler(labeler):
 
         super().__init__(bar_data)
 
-    def getDailyVolatility(close_data, span0=100):
+    def getDailyVolatility(self, close_data=None, span0=100):
         '''
         Compute the daily volatility of a stock.
         :param close_data: pandas.DataFrame, the close price of the stock
@@ -106,6 +108,9 @@ class tripleBarrierLabeler(labeler):
 
         From Page 44 of Advances in Financial Machine Learning by Marcos Lopez de Prado
         '''
+        if close_data is None:
+            close_data = self.bar_data['close_price']
+
         df0 = close_data.index.searchsorted(close_data.index - pd.Timedelta(days=1))
         df0 = df0[df0 > 0]
         df0 = pd.Series(close_data.index[df0 - 1], index=close_data.index[close_data.shape[0] - df0.shape[0]:])
@@ -113,3 +118,50 @@ class tripleBarrierLabeler(labeler):
         df0 = df0.ewm(span=span0).std()
         return df0
     
+    def label(self, close_data=None, upFactor=1, downFactor=1, horizon=60*7, binary=True):
+        ''' 
+        Label the bar data.
+        :param close_data: pandas.DataFrame, the close price of the stock
+        :param upFactor: float, the factor to multiply the daily volatility by to get the upper barrier
+        :param downFactor: float, the factor to multiply the daily volatility by to get the lower barrier
+        :param horizon: int, the number of bars to look forward for a vertical barrier
+        :param binary: bool, whether to label the data as 1, 0, or -1 or 1, {return}, -1
+        :return: pandas.DataFrame, the labeled bar data
+        '''
+
+        if close_data is None:
+            close_data = self.bar_data['close_price']
+
+        daily_vol = self.getDailyVolatility(close_data)
+        daily_vol = daily_vol.reindex(close_data.index).ffill()
+
+        # Compute the upper and lower barriers
+        up_barrier = close_data * (1 + upFactor * daily_vol)
+        down_barrier = close_data * (1 - downFactor * daily_vol)
+
+        for i in tqdm.tqdm(range(len(self.bar_data))):
+            # if the bar is the last bar, then there is no vertical barrier
+
+            # if the bar is not within the horizon, then there is a vertical barrier
+            # loop through the days within the horizon and see if it cross the upper or lower barrier
+            crossed = False
+            for j in range(i, i + horizon):
+                if j >= len(self.bar_data) - 1:
+                    break
+                if self.bar_data.iloc[j, 5] > up_barrier[i]:
+                    self.bar_data.loc[i, 'label'] = 1
+                    crossed = True
+                    break
+                elif self.bar_data.iloc[j, 5] < down_barrier[i]:
+                    self.bar_data.loc[i, 'label'] = -1
+                    crossed = True
+                    break
+
+            # if the bar did not cross the upper or lower barrier, then it is a pass
+            if not crossed:
+                if binary:
+                    self.bar_data.loc[i, 'label'] = 0
+                else:   
+                    self.bar_data.loc[i, 'label'] = self.bar_data.loc[i + horizon, 'close_price'] / self.bar_data.loc[i, 'close_price'] - 1
+        
+        return self.bar_data
